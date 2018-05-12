f797c99fe6a0386125a763cbaa3a03aa986c1171
Suhail <suhailm0608@gmail.com>
2018-05-12 22:31:11 +0100

    Test

--- SwifflingBot.py
+++ SwifflingBot.py
@@ -703,8 +703,10 @@ async def sql():
     await person.send(con)
 
     cur = con.cursor()
+    """
     cur.execute("CREATE TABLE warnings (mention varchar, num integer)")
     cur.execute("INSERT INTO warnings VALUES (%s, %s)",(person.mention,1))
+    """
     cur.execute("SELECT * FROM warnings")
     rows = cur.fetchall()
     await person.send(rows)
