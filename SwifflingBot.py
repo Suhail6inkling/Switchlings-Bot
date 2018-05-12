056bdd248b7ea4c8b9c96d3b47c1dad86ced0d41
Suhail <suhailm0608@gmail.com>
2018-05-12 20:05:13 +0100

    I genuinely don't know what I'm doing

--- SwifflingBot.py
+++ SwifflingBot.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio, random, os, csv, time, psycopg2
-import urllib.parse as urlparse
+from flask.ext.sqlalchemy import SQLAlchemy
 
 try:
     from config import TOKEN, badwords1, badwords2, noroles, channels, SSinfo, hangmanwords, allowedwords
@@ -693,18 +693,12 @@ Letters guessed: *{}*
 
 
 async def sql():
-    global person
-    dburl = os.environ["DATABASE_URL"]
-
-    con = psycopg2.connect(dburl, sslmode="require")
-    await person.send(dburl)
-    await person.send(con)
-
-    cur = con.cursor()
-    cur.execute("CREATE DATABASE warnings")
-    cur.execute("SELECT * FROM warnings")
-    rows = cur.fetchall()
-    await person.send(rows)
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATAVASE_URI"] = os.environ["DATABASE_URL"]
+    db = SQLAlchemy(app)
+    from app import db
+    db.create_all()
+    await person.send("Success? Hopefully?")
 
 @client.event
 async def on_member_remove(member):
