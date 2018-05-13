9f7fb17248f6cad17818944691d31d6de49febcc
Suhail <suhailm0608@gmail.com>
2018-05-13 10:09:30 +0100

    Almost genius, but not exactly

--- SwifflingBot.py
+++ SwifflingBot.py
@@ -696,7 +696,7 @@ class sql():
                 cur.execute("INSERT INTO warnings VALUES (%s, %s)",(warningeelist[0],warningeelist[1]))
 
         else:
-            cur.execute("DELETE FROM warnings WHERE mention = {}".format(warningeelist[0]))        
+            cur.execute("DELETE FROM warnings WHERE mention = %s",warningeelist[0])        
         await sql.close()
 
 
