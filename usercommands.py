bf40975595e7e0775094019dcd0d7e4481fb6959
Suhail <suhailm0608@gmail.com>
2018-06-03 15:29:09 +0100

    Final fix :3

--- usercommands.py
+++ usercommands.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import asyncio
 import random
-from SwifflingBot import TOKEN, badwords1, badwords2, noroles, channels, SSinfo, hangmanwords, allowedwords, TCK, TCS, TATC, TATS
+from SwifflingBot import onlinestuff, TOKEN, badwords1, badwords2, noroles, channels, SSinfo, hangmanwords, allowedwords, TCK, TCS, TATC, TATS
 
 server, starttime, person, ownrole, grouprole, welcomechat, swifflingbotchat, warningschat, bot, defmaster, api = onlinestuff()
 class UserCommands():
