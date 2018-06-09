9c5e1af4ec1e8e895d11fed8891bcb2219eced2a
Suhail <suhailm0608@gmail.com>
2018-06-09 02:40:49 +0100

    Forgot to synchronise open

--- sql.py
+++ sql.py
@@ -1,6 +1,6 @@
 import asyncio, random, os, psycopg2, discord
 
-async def open():
+def open():
     global con, cur
     dburl = os.environ["HEROKU_POSTGRESQL_COBALT_URL"]
     con = psycopg2.connect(dburl, sslmode="require")
@@ -11,10 +11,12 @@ def run(task):
     cur.execute(task)
 
 def read():
+    global cur
     cur.execute("SELECT * FROM people")
     return cur.fetchall()
 
 def close():
+    global cur,conn
     con.commit()
     cur.close()
     con.close()
