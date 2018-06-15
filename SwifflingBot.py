9d9bdd6c563a96c1dcf136febd56098d36ee05ec
Suhail <suhailm0608@gmail.com>
2018-06-15 14:52:18 +0100

    Edit

--- SwifflingBot.py
+++ SwifflingBot.py
@@ -103,7 +103,7 @@ sheet.insert_row(values,index=1)
 a = []
 for x in people:
     v = []
-    for y in range(0, len(people[x])):
+    for y in range(0, len(list(people[x]))):
         v.appned(str(people[x][y]))
     a.append(v)
 for x in range(0,100):
