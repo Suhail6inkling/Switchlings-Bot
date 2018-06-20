3d9b88971e78d64a68104709bb113afa8f2a5f5b
Suhail <suhailm0608@gmail.com>
2018-06-20 19:10:47 +0100

    Splatfest edit

--- splatfestcommands.py
+++ splatfestcommands.py
@@ -31,8 +31,49 @@ class SplatfestCommands():
     def __init__(self, client):
         self.client = client
 
-    @commands.command(pass_context=True)
-    async def lastsplatfest(self, ctx, region=""):
+    @commands.group(pass_context=True)
+    async def splatfest(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send("Please choose from one of the following options:\n```md\n<s.splatfest upcoming (region)> - View the upcoming or ongoing Splatfest\n<s.splatfest recent (region)> - View stats of the most recent Splatfest\n<s.splatfest list (region)> - View overall stats of the most recent Splatfests```")
+
+    @splatfest.command(pass_context=True)
+    async def list(self, ctx, region=""):
+        regions = ["na","eu","jp"]
+        regiontimezones = {"na": [["PDT",-25200],["EDT",-14400]], "eu" : [["BST",3600],["CEST",7200]], "jp": [["JST",32400]]}
+        if region == "" or region.lower() not in regions:
+            await ctx.send("""Please select either:\nna -> North America (& Oceania)\neu -> Europe\njp -> Japan""")
+            return
+        region = region.lower()
+        req = Request(url, headers={"User-Agent": "Mozilla/5.0"})
+        web_byte = urlopen(req).read()
+        webpage = web_byte.decode("utf-8")
+        allmodes = json.loads(webpage)
+        splatfests = allmodes[region]["festivals"]
+        description = "`"
+        for x in range(0,5):
+            a = splatfests[x]
+            alpha = a["names"]["alpha_short"]
+            bravo = a["names"]["bravo_short"]
+            fid = a["festival_id"]
+            alphacount=0
+            bravocount=0
+            ourresult = [d for d in allmodes[results] if d["festival_id"] == fid][0]
+            for x in ourresult["summary"]:
+                if ourresult["summary"][x] ==0:
+                    if x != "total":
+                        alphacount+=1
+
+                else:
+                    if x != "total":
+                        bravocount+=1
+            description = "{desc}{a:20} {as:5}-{bs:5} {b:20}\n".format(desc=description,a=alpha,as=alphacount,bs=bravocount,b=bravo)
+       description="{}`".format(description)
+       embed = discord.Embed(title="Recent Splatfests",colour=0x2d6092,description=description)
+       embed.set_thumbnail(url="https://cdn.wikimg.net/en/splatoonwiki/images/thumb/9/9a/S2_Splatfest_Logo.svg/512px-S2_Splatfest_Logo.svg.png")
+       await ctx.send(embed=embed)
+       
+    @splatfest.command(pass_context=True)
+    async def recent(self, ctx, region=""):
         regions = ["na","eu","jp"]
         regiontimezones = {"na": [["PDT",-25200],["EDT",-14400]], "eu" : [["BST",3600],["CEST",7200]], "jp": [["JST",32400]]}
         if region == "" or region.lower() not in regions:
@@ -44,7 +85,6 @@ class SplatfestCommands():
         webpage = web_byte.decode("utf-8")
         allmodes = json.loads(webpage)
         splatfest = allmodes[region]["festivals"][0]
-        timenow = time.time()
         if splatfest["times"]["end"] < timenow and splatfest["times"]["result"] > timenow:
             await ctx.send("The results of Team {} vs Team {} have not been announced... Showing previous Splatfest...".format(splatfest["names"]["alpha_short"],splatfest["names"]["bravo_short"]))
         if splatfest["times"]["result"] > timenow:
@@ -57,6 +97,12 @@ class SplatfestCommands():
         middlehex = hexcolor(splatfest["colors"]["middle"])
         mainimage = "https://splatoon2.ink/assets/splatnet{}".format(splatfest["images"]["panel"])
         results = allmodes[region]["results"]
+        description=""
+        for x in regiontimezones[region]:
+            startdate = time.strftime("%d %b %H:%M:00",time.gmtime(starttime+x[1]))
+            enddate = time.strftime("%d %b %H:%M:00",time.gmtime(endtime+x[1]))
+            date = "{} - {}".format(startdate,enddate)
+            description = "{}{}\n".format(description,date)
         ourresult = [d for d in results if d["festival_id"]==eyedee]
         if ourresult==[]:
             await ctx.send("There was an error processing your request")
@@ -92,7 +138,7 @@ class SplatfestCommands():
         t4 = p.format(str(float(t["team"]["alpha"])/100),"Team Wins",str(float(t["team"]["bravo"])/100))
         t5 = p.format(alphacount,"Total",bravocount)
         
-        description = "`{}\n{}\n{}\n{}\n{}`\n\nTeam {} wins!".format(t1,t2,t3,t4,t5,winner)
+        description = "{}`{}\n{}\n{}\n{}\n{}`\n\nTeam {} wins!".format(description,t1,t2,t3,t4,t5,winner)
         embed = discord.Embed(title="Splatfest Results",description=description, colour=middlehex)
         embed.set_thumbnail(url=mainimage)
         await ctx.send(embed=embed)
@@ -101,8 +147,8 @@ class SplatfestCommands():
 
 
 
-    @commands.command(pass_context=True)
-    async def nextsplatfest(self, ctx, region=""):
+    @splatfest.command(pass_context=True)
+    async def next(self, ctx, region=""):
         regions = ["na","eu","jp"]
         regiontimezones = {"na": [["PDT",-25200],["EDT",-14400]], "eu" : [["BST",3600],["CEST",7200]], "jp": [["JST",32400]]}
         if region == "" or region.lower() not in regions:
