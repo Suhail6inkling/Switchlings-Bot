27db8059537efef8f0cb7079f2d28a3c6daf6ae0
Suhail <suhailm0608@gmail.com>
2018-06-20 22:44:30 +0100

    O

--- gsheets.py
+++ gsheets.py
@@ -23,13 +23,13 @@ async def redoplaceinqueue():
     for x in range(0,lentimes):
         for y in range((50*x),(50*x)+49):
             if y !=0 and y !=1:
-                placebat="{}{}".format("A",str(x))
-                sheet.update_acell(placebat,str(x))
+                placebat="{}{}".format("A",str(y))
+                sheet.update_acell(placebat,str(y))
         await asyncio.sleep(100)
     for y in range(50*(x+1),lenny):
         if y!=0 and y!=1:
-            placebat="{}{}".format("A",str(x))
-            sheet.update_acell(placebat,str(x))
+            placebat="{}{}".format("A",str(y))
+            sheet.update_acell(placebat,str(y))
         
 
 
