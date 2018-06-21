5313534e009e028c918c85446928da260cdaf3f2
Suhail <suhailm0608@gmail.com>
2018-06-21 18:41:25 +0100

    edit

--- rankcommands.py
+++ rankcommands.py
@@ -43,28 +43,32 @@ class RankCommands():
 
     @commands.command(pass_context=True)
     async def addrank(self, ctx, *, rankname: str):
-        LOR.open()
-        ranks = LOR.read()
-        server = ctx.guild
-        if rankname.title() in [role.name for role in server.roles]:
-            if rankname.title() in ranks:
-                await ctx.send("That role is already a rank.")
-                return
-            ranks.append(rankname.title())
-            LOR.updaterow(ranks)
-            await ctx.message.add_reaction("✅")
-        else:
-            await ctx.send("Error. That role doesn't exist in the server.")
+        if "The Switchlings" in [role.name for role in ctx.author.roles]:
+            LOR.open()
+            ranks = LOR.read()
+            server = ctx.guild
+            if rankname.title() in [role.name for role in server.roles]:
+                if rankname.title() in ranks:
+                    await ctx.send("That role is already a rank.")
+                    return
+                ranks.append(rankname.title())
+                LOR.updaterow(ranks)
+                await ctx.message.add_reaction("✅")
+            else:
+                await ctx.send("Error. That role doesn't exist in the server.")
     
     @commands.command(pass_context=True)
     async def delrank(self, ctx, *, rankname: str):
-        LOR.open()
-        ranks = LOR.read()
-        server = ctx.guild
-        if rankname.title() in ranks:
-            ranks.remove(rankname.title())
-            LOR.updaterow(ranks)
-            await ctx.message.add_reaction("✅")
+        if "The Switchlings" in [role.name for role in ctx.author.roles]:
+            LOR.open()
+            ranks = LOR.read()
+            server = ctx.guild
+            if rankname.title() in ranks:
+                ranks.remove(rankname.title())
+                LOR.updaterow(ranks)
+                await ctx.message.add_reaction("✅")
+            else:
+                await ctx.send("That rank doesn't exist.")
 
 
 
